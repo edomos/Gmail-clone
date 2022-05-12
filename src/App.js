5fec55139744722a8ff4d05214d52b40ce1eba49
edomos <alf.ramoz@gmail.com>
2022-05-12 00:52:06 -0700

    file cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-      <h2>Let''s build gmail</h2>
+      <Header />
+      <h2>Let's build gmail</h2>
     </div>
   );
 }
