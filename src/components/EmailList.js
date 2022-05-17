fa207c3ce3362d51da3e87546ab16873d2e7d1bd
edomos <alf.ramoz@gmail.com>
2022-05-17 00:09:04 -0700

    - create email list sections with styling - adjust sidebar width

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -8,6 +8,10 @@ import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
 import Settings from "@mui/icons-material/Settings";
 import React from "react";
 import "./EmailList.css";
+import Section from "./Section"
+import InboxIcon from '@mui/icons-material/Inbox';
+import PeopleIcon from '@mui/icons-material/People';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
 function EmailList() {
   return (
@@ -39,6 +43,14 @@ function EmailList() {
                <Settings />
            </IconButton>
         </div>
+      </div>
+      <div className="emailList__sections">
+          <Section Icon={InboxIcon} title="Primary" color="red" selected />
+          <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+          <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+      <div className="emailList__list">
+          
       </div>
     </div>
   );
