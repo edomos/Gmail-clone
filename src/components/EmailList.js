6958c2c39fdbe4d2c46ae997dbace8a4207e3321
edomos <alf.ramoz@gmail.com>
2022-05-16 23:44:36 -0700

    create additional email list icons

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -2,6 +2,10 @@ import { Checkbox, IconButton } from "@mui/material";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import RedoIcon from '@mui/icons-material/Redo';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
+import Settings from "@mui/icons-material/Settings";
 import React from "react";
 import "./EmailList.css";
 
@@ -19,6 +23,20 @@ function EmailList() {
            </IconButton>
            <IconButton>
                <MoreVertIcon />
+           </IconButton>
+        </div>
+        <div className="emailList__settings--right">
+           <IconButton>
+               <KeyboardArrowLeftIcon />
+           </IconButton>
+           <IconButton>
+               <KeyboardArrowRightIcon />
+           </IconButton>
+           <IconButton>
+               <KeyboardHideIcon />
+           </IconButton>
+           <IconButton>
+               <Settings />
            </IconButton>
         </div>
       </div>
