b0a58676f58f6dbe1677971f2d8b1d4938bd6160
edomos <alf.ramoz@gmail.com>
2022-05-17 22:37:34 -0700

    create mail tool icons for left side of mail page

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,9 +1,56 @@
+import { ArrowBack, CheckCircle, Delete, Email, Error, MoveToInbox, WatchLater } from '@mui/icons-material';
+import LabelImportant from '@mui/icons-material/LabelImportant';
+import MoreVert from '@mui/icons-material/MoreVert';
+import { IconButton } from '@mui/material';
 import React from 'react';
 import "./Mail.css"
 
 function Mail() {
     return (
         <div className='mail'>
+            <div className="mail__tools">
+                <div className="mail__tools--left">
+                <IconButton>
+                    <ArrowBack />
+                </IconButton>
+
+                <IconButton>
+                    <MoveToInbox />
+                </IconButton>
+
+                <IconButton>
+                    <Error />
+                </IconButton>
+
+                <IconButton>
+                    <Delete />
+                </IconButton>
+
+                <IconButton>
+                    <Email />
+                </IconButton>
+
+                <IconButton>
+                    <WatchLater />
+                </IconButton>
+
+                <IconButton>
+                    <CheckCircle />
+                </IconButton>
+
+                <IconButton>
+                    <LabelImportant />
+                </IconButton>
+
+                <IconButton>
+                    <MoreVert />
+                </IconButton>
+
+                </div>
+                <div className="mail__tools--right">
+
+                </div>
+            </div>
             <h1>Mail Page</h1>
         </div>
     )
